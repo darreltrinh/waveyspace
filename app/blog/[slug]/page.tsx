6aa465031398fb70cc768ef20a925cc2222f63d0
darreltrinh <darrel.w.trinh@gmail.com>
2023-05-20 18:41:13 -0400

    assign url in vercel for build

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,37 +1,37 @@
-export const revalidate = 420; // ISR implementation
+// export const revalidate = 420; // ISR implementation
 
-interface Post {
-    title: string;
-    content: string;
-    slug: string;
-}
+// interface Post {
+//     title: string;
+//     content: string;
+//     slug: string;
+// }
 
-// for dynamic data that doesn't change often (ie. blog posts)
-export async function generateStaticParams() {
-    const posts: Post[] = await fetch('https://waveyspace.vercel.app/api/content').then(
-        (res) => res.json()
-    );
+// // for dynamic data that doesn't change often (ie. blog posts)
+// export async function generateStaticParams() {
+//     const posts: Post[] = await fetch('https://waveyspace.vercel.app/api/content').then(
+//         (res) => res.json()
+//     );
 
-    return posts.map((post) => ({
-        slug: post.slug,
-    }))
-}
+//     return posts.map((post) => ({
+//         slug: post.slug,
+//     }))
+// }
 
-interface Props {
-    params: {slug: string};
-}
+// interface Props {
+//     params: {slug: string};
+// }
 
-export default async function BlogPostPage({params}: Props) {
-    // deduped
-    const posts: Post[] = await fetch('https://waveyspace.vercel.app/api/content').then(
-        (res) => res.json()
-    )
-    const post = posts.find((post) => post.slug === params.slug)!;
+// export default async function BlogPostPage({params}: Props) {
+//     // deduped
+//     const posts: Post[] = await fetch('https://waveyspace.vercel.app/api/content').then(
+//         (res) => res.json()
+//     )
+//     const post = posts.find((post) => post.slug === params.slug)!;
     
-    return (
-        <div>
-            <h1>{post.title}</h1>
-            <h1>{post.content}</h1>
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             <h1>{post.title}</h1>
+//             <h1>{post.content}</h1>
+//         </div>
+//     )
+// }
